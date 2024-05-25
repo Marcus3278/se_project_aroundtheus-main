fffaf46ef61c2aadcc9b1723948be2705d99e014
Obi-Pi-Kenobi <marcuscrawford91@gmail.com>
2024-05-25 10:26:14 -0500

    Update PopupWithForm.js

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,59 +6,69 @@ export default class PopupWithForm extends Popup {
     super({ popupSelector });
 
     // Ensure the popup element exists
-    if (!this._popupElement) {
+    if (!this.popupElement) {
       throw new Error(`Popup element with selector "${popupSelector}" not found`);
     }
 
     // Cache selectors for efficiency
-    this._popupForm = this._popupElement.querySelector(selectors.popupForm);
-    this._inputElements = this._popupForm.querySelectorAll(selectors.popupInput);
-    this._submitButtonElement = this._popupElement.querySelector(selectors.submitButtonSelector);
+    this.popupForm = this.popupElement.querySelector(selectors.popupForm);
+    this.inputElements = this.popupForm.querySelectorAll(selectors.popupInput);
+    this.submitButtonElement = this.popupElement.querySelector(selectors.submitButtonSelector);
 
-    if (!this._popupForm) {
+    if (!this.popupForm) {
       throw new Error(`Form element with selector "${selectors.popupForm}" not found`);
     }
-    if (!this._submitButtonElement) {
+    if (!this.submitButtonElement) {
       throw new Error(`Submit button with selector "${selectors.submitButtonSelector}" not found`);
     }
 
-    this._handleFormSubmit = handleFormSubmit;
-    if (typeof this._handleFormSubmit !== 'function') {
+    this.handleFormSubmit = handleFormSubmit;
+    if (typeof this.handleFormSubmit !== 'function') {
       throw new Error('handleFormSubmit should be a function');
     }
 
-    this._progressButtonText = selectors.savingButtonText || 'Saving...';
-    this._defaultButtonText = selectors.saveButtonDefaultText || 'Save';
+    this.progressButtonText = selectors.savingButtonText || 'Saving...';
+    this.defaultButtonText = selectors.saveButtonDefaultText || 'Save';
   }
 
-  // Private method to get input values from the form
-  _getInputValues() {
+  // Method to get input values from the form
+  getInputValues() {
     const inputValues = {};
-    this._inputElements.forEach(element => {
+    this.inputElements.forEach(element => {
       inputValues[element.name] = element.value;
     });
     return inputValues;
   }
 
+  // Method to set input values in the form
+  setInputValues(data) {
+    this.inputElements.forEach(input => {
+      input.value = data[input.name];
+    });
+  }
+
   // Method to set event listeners
   setEventListeners() {
-    this._popupForm.addEventListener("submit", (event) => {
+    this.popupForm.addEventListener("submit", (event) => {
       event.preventDefault();
-      this._handleFormSubmit(this._getInputValues());
+      this.handleFormSubmit(this.getInputValues());
     });
     super.setEventListeners();
   }
 
   // Method to show button progress
-  showButtonProgress(showButtonProgress) {
-    if (typeof showButtonProgress !== 'boolean') {
-      throw new Error('showButtonProgress should be a boolean');
-    }
-    this._submitButtonElement.textContent = showButtonProgress ? this._progressButtonText : this._defaultButtonText;
+  renderLoading(isLoading, loadingText = this.progressButtonText) {
+    this.submitButtonElement.textContent = isLoading ? loadingText : this.defaultButtonText;
   }
 
   // Method to reset the form
   reset() {
-    this._popupForm.reset();
+    this.popupForm.reset();
+  }
+
+  close() {
+    super.close();
+    this.reset();
   }
 }
+
